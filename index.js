3b88bc4d0e1986c5f77432f156dc79f2732bf619
Julian Toscani <julian_toscani@gmx.de>
2021-07-10 19:55:47 +0200

    extend indexjs

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import handleFileChange from "./scripts/handleFileChange.js";
+import setUpTemplate from "./scripts/setUpTemplate.js";
+
+setUpTemplate();
 
 const fileInput = document.querySelector("#files");
 fileInput.addEventListener("change", handleFileChange);
