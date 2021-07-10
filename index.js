d4ce077231a7aca01858d95b746b487e45c3e014
Julian Toscani <julian_toscani@gmx.de>
2021-07-10 23:02:00 +0200

    implement download script

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import handleFileChange from "./scripts/handleFileChange.js";
+import handleFileCreation from "./scripts/handleFileCreation.js";
 import setUpTemplate from "./scripts/setUpTemplate.js";
 
 setUpTemplate();
 
 const fileInput = document.querySelector("#files");
 fileInput.addEventListener("change", handleFileChange);
+
+const submitButton = document.querySelector("button[type=submit]");
+submitButton.addEventListener("click", handleFileCreation);
